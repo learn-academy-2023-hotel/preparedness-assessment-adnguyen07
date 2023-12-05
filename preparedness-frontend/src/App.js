b226d2891d114d53d67c334d1d189833666a1453
alexnguyen <alexanderdnguyen4@gmail.com>
2023-12-05 13:02:14 -0800

    Initial commit

--- preparedness-frontend/src/App.js
+++ preparedness-frontend/src/App.js
@@ -1,20 +1,27 @@
-import React from "react"
+import React, {useState} from "react"
 import { Button, Input, Label } from "reactstrap"
 import ModalComponent from "./components/ModalComponent"
 import "./App.css"
 
 const App = () => {
+
+  const [modal, setModal] = useState(false)
+
+  const toggle = () => setModal(!modal)
+
+  const [name, setName] = useState("")
+
   return (
     <div className="entire-content">
       <h1>Preparedness Assessment</h1>
       <div className="form">
         <div className="input">
           <Label for="name">Enter your name</Label>
-          <Input />
+          <Input value={name} onChange={e => setName(e.target.value)}/>
         </div>
-        <Button>Click Me</Button>
+        <Button onClick={toggle}>Click Me</Button>
         <Button>Reset</Button>
-        <ModalComponent />
+        <ModalComponent modal={modal} toggle={toggle} name={name}/>
       </div>
     </div>
   )
--- preparedness-frontend/src/components/ModalComponent.js
+++ preparedness-frontend/src/components/ModalComponent.js
@@ -1,7 +1,23 @@
 import React from "react"
+import {Button, Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap'
 
-const ModalComponent = () => {
-  return <></>
+const ModalComponent = (props) => {
+  
+  return (
+    <>
+    <div>
+      <Modal isOpen={props.modal} toggle={props.toggle}>
+        <ModalHeader toggle={props.toggle}>Hello {props.name}!</ModalHeader>
+        <ModalBody>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Neque sodales ut etiam sit amet nisl. Bibendum ut tristique et egestas quis ipsum. Lacus sed turpis tincidunt id aliquet risus. Integer enim neque volutpat ac tincidunt vitae semper quis lectus. Sed viverra tellus in hac habitasse platea. Id venenatis a condimentum vitae. Vitae congue eu consequat ac felis donec. Non nisi est sit amet facilisis. Id aliquet lectus proin nibh nisl condimentum id. Ornare quam viverra orci sagittis eu volutpat. Tempus imperdiet nulla malesuada pellentesque. A arcu cursus vitae congue mauris. Ante in nibh mauris cursus. Vulputate eu scelerisque felis imperdiet proin fermentum leo vel.
+        </ModalBody>
+        <ModalFooter>
+          <Button onClick={props.toggle}>Close</Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+    </>
+  )
 }
 
 export default ModalComponent
